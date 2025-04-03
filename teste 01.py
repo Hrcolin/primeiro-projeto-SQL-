2015b059bcb89082bf987a189cf3b8d7a7b9b12e
Haroldo  Colin <166832664+Hrcolin@users.noreply.github.com>
2025-04-03 17:27:42 -0300

    Add files via upload

---
+++ teste 01.py
@@ -0,0 +1,13 @@
+def eh_primo(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Digite um número: "))
+if eh_primo(num):
+    print(f"{num} é um número primo.")
+else:
+    print(f"{num} não é um número primo.")
